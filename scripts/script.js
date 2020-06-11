9ad423618bcebe1c2acb2cea51304bd55400da48
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-06-11 08:31:25 -0400

    wrote JS pseudocode

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+// Create namespace object
+// Document Ready
+// Init Function
+// Create variable DOM selectors
+// Begin button needs id to scroll to main section
+
+
+// Landmark Submit button -> Create Event Listener
+//  Create parks and schools arrays -> five different options
+//  .each() through the array, and render to DOM
+
+// Dropdown Event Listener
+//  make ajax call -> then method takes results
+//  Create variables for each piece of data needed
+//  Lat/Long Function used
+//  Populate html tags with data variables
+//  For ul, we'll need <li> with <h3>, <p>, <ul> with <li><i></li>
+//  Toggle class to make display section visible
+
+// Reset Button -> Event Listener
+//  Toggle Class of display section to make invisible
+//  Reset Forms
+
+// NAMESPACE OBJECT
+const app = {};
+
+
+
+
+
+
+
+
+// INIT FUNCTION
+app.init = function() {
+
+}
+
+// DOCUMENT READY
+$(document).ready(function () {
+  app.init();
+});
