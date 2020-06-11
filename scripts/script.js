21672442cb53497af50868394fb5c1f2834a43ed
Jahanban <jahanban@gmail.com>
2020-06-11 15:37:08 -0400

    Schools and Parks array long and lat updated

--- scripts/script.js
+++ scripts/script.js
@@ -25,31 +25,63 @@
 const app = {};
 
 const schoolsArray = [
-  {
-    name: "Juno",
-    lat: "",
-    long: ""
-  },
-  {
-    name: "University of Toronto - St. George",
-    lat: "",
-    long: ""
-  }, 
-  {
-    name: "George Brown - St. James",
-    lat: "",
-    long: ""
-  }, 
-  {
-    name: "Ryerson",
-    lat: "",
-    long: ""
-  }, 
-  {
-    name: "OCAD",
-    lat: "",
-    long: ""
-  },  
+    {
+        name: "Juno",
+        lat: 43.6483,
+        long: 79.3979
+    },
+    {
+        name: "University of Toronto - St. George",
+        lat: 43.6629,
+        long: 79.3957
+    },
+    {
+        name: "George Brown - St. James",
+        lat: 43.6513,
+        long: 79.3702
+    },
+    {
+        name: "Ryerson",
+        lat: 43.6577,
+        long: 79.3788
+    },
+    {
+        name: "OCAD",
+        lat: 43.6530,
+        long: 79.3912
+    }
+]
+
+const parksArray = [
+    {
+        name: "High Park",
+        lat: 43.6465,
+        long: 79.4637
+    },
+
+    {
+        name: "Riverdale Park East",
+        lat: 43.6708,
+        long: 79.3561
+    },
+
+    {
+        name: "Christie Pits Park",
+        lat: 43.6646,
+        long: 79.4207
+    },
+
+    {
+        name: "Toronto Music Garden",
+        lat: 43.636927,
+        long: 79.394655
+    },
+
+    {
+        name: "Underpass Park",
+        lat: 43.6560117,
+        long: 79.355092
+    }
 ]
 
 
@@ -58,28 +90,27 @@ const schoolsArray = [
 
 
 
-
 // INIT FUNCTION
-app.init = function() {
+app.init = function () {
 
-  const $formLandmark = $('.form-landmark');
-  const $parksRadio = $('.parks-radio');
-  const $schoolsRadio = $('.schools-radio');
-  const $buttonLandmark = $('.button-landmark');
+    const $formLandmark = $('.form-landmark');
+    const $parksRadio = $('.parks-radio');
+    const $schoolsRadio = $('.schools-radio');
+    const $buttonLandmark = $('.button-landmark');
 
-  const $formLocation = $('.form-location'); 
-  const $selectLocation = $('.select-location');
+    const $formLocation = $('.form-location');
+    const $selectLocation = $('.select-location');
 
-  const $bikeInfo = $('.bike-info');
-  const $infoLocationName = $('.info-location-name');
-  const $infoLocationImage = $('.info-location-image');
-  const $infoStationList = $('.info-station-list');
+    const $bikeInfo = $('.bike-info');
+    const $infoLocationName = $('.info-location-name');
+    const $infoLocationImage = $('.info-location-image');
+    const $infoStationList = $('.info-station-list');
 
-  const $buttonReset = $('.button-reset');
+    const $buttonReset = $('.button-reset');
 
 }
 
 // DOCUMENT READY
 $(document).ready(function () {
-  app.init();
+    app.init();
 });
